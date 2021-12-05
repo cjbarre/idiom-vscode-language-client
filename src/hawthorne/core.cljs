c4ae3b5a50a1105764c849958c418161527466c6
Brandon Ringe <brandon.ringe@gmail.com>
2021-12-04 19:22:35 -0800

    Add reloadable code - WIP

--- src/hawthorne/core.cljs
+++ src/hawthorne/core.cljs
@@ -1,14 +1,38 @@
 (ns hawthorne.core
   (:require ["vscode" :as vscode :refer [window]]))
 
+(defonce current-context (atom nil))
+(defonce disposables (atom []))
+
+(defn add-disposable! [disposable]
+  (swap! disposables conj disposable))
+
+;; TODO: Complete this function
+(defn dispose-disposables! [])
+
 (defn say-hello []
-  (.. window (showInformationMessage "Hello world!!")))
+  (.. window (showInformationMessage "Hello world!")))
 
 (defn activate
   [^js context]
+  (reset! current-context context)
   (js/console.log "Hawthorne activating")
-  (.. vscode -commands (registerCommand "hawthorne.sayHello" say-hello)))
+  ;; TODO: Add this to disposables atom
+  (.. context -subscriptions
+      (push (.. vscode -commands
+                (registerCommand "hawthorne.sayHello" say-hello)))))
 
+;; TODO: Call dispose-disposables!
 (defn deactivate
   []
   (js/console.log "Hawthorne deactivating"))
+
+(defn before-load-async [done]
+  (prn "running before-load")
+  (deactivate)
+  (done))
+
+(defn after-load []
+  (prn "running after-load")
+  (activate @current-context)
+  (println "Reloaded"))
